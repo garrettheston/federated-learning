05d2608c5def518fba7b0aec2b6c4031bdce7614
garrettheston <glh0007@uah.edu>
2024-10-07 18:25:09 -0500

    Updated symmetric key algorithm prototype - symmetric key encryption for each individual model update.

--- main_fed.py
+++ main_fed.py
@@ -82,40 +82,44 @@ if __name__ == '__main__':
     net_best = None
     best_loss = None
     val_acc_list, net_list = [], []
-
-    # Generating key pair
-    ek, dk = ML_KEM_512.keygen()
-    # Generating shared secret key
-    shared_key_sender, ct = ML_KEM_512.encaps(ek)
-    # Key decapsulation
-    shared_key_receiver = ML_KEM_512.decaps(dk, ct)
-    assert shared_key_sender == shared_key_receiver
-
-    iv = get_random_bytes(16)  # Generate random IV
-    
-    # Create distinct ciphers for both encryption and decryption
-    init_cipher = AES.new(shared_key_sender[:32], AES.MODE_OFB, iv)
-    another_cipher = AES.new(shared_key_receiver[:32], AES.MODE_OFB, iv)
     
     # Hex digests before and after transmission
     pre_transmission_vector = []
     post_transmission_vector = []
-    
     # Validae alteration
     altered = False
-    
     ciphertext = ""
+    idxs_users = ""
     
     if args.all_clients: 
         print("Aggregation over all clients")
         w_locals = [w_glob for i in range(args.num_users)]
     for iter in range(args.epochs):
+        # Shared_key_receiver session key and initialization vector (iv)
+        session_key_and_iv = []
         loss_locals = []
         if not args.all_clients:
             w_locals = []
         m = max(int(args.frac * args.num_users), 1)
         idxs_users = np.random.choice(range(args.num_users), m, replace=False)
         for idx in idxs_users:
+
+            # Generating session keypair
+            ek, dk = ML_KEM_512.keygen()
+            # Generating shared secret key
+            shared_key_sender, ct = ML_KEM_512.encaps(ek)
+            # Key decapsulation
+            shared_key_receiver = ML_KEM_512.decaps(dk, ct)
+            assert shared_key_sender == shared_key_receiver
+
+            iv = get_random_bytes(16)  # Generate random IV
+
+            # Derive cipher for encryption from PEM
+            init_cipher = AES.new(shared_key_sender[:32], AES.MODE_OFB, iv)
+            
+            # Append the receiver key and the iv to the dictionary
+            session_key_and_iv.append((shared_key_receiver,iv))
+            
             local = LocalUpdate(args=args, dataset=dataset_train, idxs=dict_users[idx])
             w, loss = local.train(net=copy.deepcopy(net_glob).to(args.device))
             
@@ -134,7 +138,9 @@ if __name__ == '__main__':
         
         # update global weights with plaintext
         for i in range(len(w_locals)):
-            decrypted = another_cipher.decrypt(w_locals[i])
+            shared_key_receiver, iv = session_key_and_iv[i]
+            new_cipher = AES.new(shared_key_receiver[:32], AES.MODE_OFB, iv)
+            decrypted = new_cipher.decrypt(w_locals[i])
             hash_value = hashlib.sha256(decrypted).hexdigest() # post-transmission hex digest
             post_transmission_vector.append(hash_value)
             w_locals[i] = pickle.loads(decrypted)
@@ -149,7 +155,7 @@ if __name__ == '__main__':
         print('Round {:3d}, Average loss {:.3f}'.format(iter, loss_avg))
         loss_train.append(loss_avg)
 
-    # intrusion detection for digests
+    # intrusion detection established by comparing sha256 hex digests before transmission and after transmission
     for i in range(len(w_locals)):
         assert pre_transmission_vector[i] == post_transmission_vector[i]
         
@@ -166,3 +172,15 @@ if __name__ == '__main__':
     print("Training accuracy: {:.2f}".format(acc_train))
     print("Testing accuracy: {:.2f}".format(acc_test))
 
+
+
+IV:  b'\x9e\x9d\x91\x82\x88\x83-\xf9c\xbao\xef\x99\xd2p\xd7'
+IV:  b'\x91`9:N\xd5@\xee\xa0\xad\xe4d6e\x92M'
+IV:  b'L\xfe\x8dJ\x90\xc2nm\x1dk:\xa55u\xd1%'
+IV:  b'\x98\x022\x84\xd8\xf9\x83\x18p\n\xf3\x9e\xb6\x01 \xaa'
+IV:  b'\xfd\x02\xec\xaa\xa4\x91\xc9\xe3\x8a\xbb\xbf}D\xa7\xf02'
+IV:  b'\xa1#\xd5.\xe5\xd80fA\x15\x0e\x9a\x83/\xde\x99'
+IV:  b'\xb4\xc5\xb2\x03\n\x15\x80\x1d\xebG\x1a\x91\x96\x1c\xc4\xdc'
+IV:  b'\x9eZ\x15;\x01\x92fG\x15\xdda\xb6x\xb8\x9eL'
+IV:  b'\xe0\x8d\xe2\xdcg9@\xc8L\x01=\x7f9\x82\x14\x9f'
+IV:  b"@'\xf8\xb9\x05\xbf\xd6\xf65\xf4\xac\xd2<\x08\xfd\xe1"
